7b76c68059af7b952ea99b132c10a3926c3ca58f
uwelov <alisabeketova@gmail.com>
2022-10-30 02:27:26 +0100

    add info

--- ________.ino
+++ ________.ino
@@ -25,17 +25,17 @@ int cookingT;
 unsigned int TimeStartCooking = 0;
 bool ReadyToCooking = false;
 float tempC; // temperature in Celsius
-/////
+
 unsigned int ConvertToMS(unsigned int minutes){
   return minutes * 60 * 1000;
 }
-/////
+
 void Select(unsigned int inputTime, int inputTemp){
   cookingTime = ConvertToMS(inputTime);
   cookingT = inputTemp;
   cooking = true;
 }
-/////
+
 void handleNewMessages(int numNewMessages){
   DS18B20.requestTemperatures();       // send the command to get temperatures
   tempC = DS18B20.getTempCByIndex(0);  // read temperature in °C
@@ -53,18 +53,18 @@ void handleNewMessages(int numNewMessages){
     String from_name = bot.messages[i].from_name;
     if (from_name == "")
       from_name = "Guest";
-    if (text == "/status"){
-        bot.sendMessage(chat_id, String(tempC)+"°C", "");
-        if(cooking && !TimeStartCooking){
-          bot.sendMessage(chat_id, "Залишилось "+String(((cookingTime * 60000) - (millis())-TimeStartCooking) / 60000)+ " хвилин до повного приготування",  "");
-        }
-      }
-    else if (text == "/start "){
+    if (text == "/start"){
       String welcome = "Привіт, " + from_name + ". Мене звати Єва, я бот-помічник розумної йогуртниці.\n";
-      bot.sendMessage(chat_id, welcome, "Markdown");
+      bot.sendMessage(chat_id, welcome, "");
       welcome = "/help - для детальної інформації команд";
-      bot.sendMessage(chat_id, welcome, "Markdown");
+      bot.sendMessage(chat_id, welcome, "");
     }
+    else if (text == "/status"){
+        bot.sendMessage(chat_id, String(tempC)+"°C", "Markdown");
+        if(cooking && !TimeStartCooking){
+          bot.sendMessage(chat_id, "Залишилось "+String((cookingTime - (millis()-TimeStartCooking)) / 60000)+ " хвилин до повного приготування",  "Markdown");
+        }
+    } 
     else if (text == "/help"){
       String helping = "/status - Перевірка часу готовності та температури\n/recipes - Каталог рецептів\n/select Назва напою (на латині) - для приготування напою\n/about Назва напою (на латині) - інформація, рецепт напою та час приготування\n/device - Про йогуртницю";
       bot.sendMessage(chat_id, helping, "Markdown");
@@ -86,80 +86,66 @@ void handleNewMessages(int numNewMessages){
     }
     else if(text.indexOf("/select") != -1){
       if(text.indexOf("Bifidok") != -1){
-        //Меняем время и температуру
         if(!cooking){
-            Select(600,22);//Select(Время в минутах, температура в С)
-            bot.sendMessage(chat_id, "Ви обрали: кефір та напій “Біфідок”", "Markdown");
+            Select(600,22);//Час у хвилинах, температура в С
+            bot.sendMessage(chat_id, "Ви обрали: кефір “Біфідок”", "Markdown");
         }
         else
-          bot.sendMessage(chat_id, "Зачекайте поки приготується попереднє замовлення", "Markdown");
+          bot.sendMessage(chat_id, "Зачекайте, поки приготується попереднє замовлення", "Markdown");
         }
-        //Дополнить алгоритмом выбора фун. части
       }
       else if(text.indexOf("Acidophilin") != -1){
-        //Меняем время и температуру
         if(!cooking){
             Select(300,32);
             bot.sendMessage(chat_id, "Ви обрали: напій “Ацидофілін”", "Markdown");
         }
-        //Дополнить алгоритмом выбора фун. части
         else{
-          bot.sendMessage(chat_id, "Зачекайте поки приготується попереднє замовлення", "Markdown");
+          bot.sendMessage(chat_id, "Зачекайте, поки приготується попереднє замовлення", "Markdown");
         }
       }
       else if(text.indexOf("Ryazhenko") != -1){
-        //Меняем время и температуру
         if(!cooking){
             Select(300,42);
             bot.sendMessage(chat_id, "Ви обрали: напій “Ряжанка”", "Markdown");
         }
-        //Дополнить алгоритмом выбора фун. части
         else{
-            bot.sendMessage(chat_id, "Зачекайте поки приготується попереднє замовлення", "Markdown");
+            bot.sendMessage(chat_id, "Зачекайте, поки приготується попереднє замовлення", "Markdown");
         }
       }
       else if(text.indexOf("BioYogurt") != -1){
-        //Меняем время и температуру
         if(!cooking){
             Select(180,42);
             bot.sendMessage(chat_id, "Ви обрали: напій “Біойогурт”", "Markdown");
         }
-        //Дополнить алгоритмом выбора фун. части
         else{
-            bot.sendMessage(chat_id, "Зачекайте поки приготується попереднє замовлення", "Markdown");
+            bot.sendMessage(chat_id, "Зачекайте, поки приготується попереднє замовлення", "Markdown");
         }
       }
       else if(text.indexOf("Acidolact") != -1){
-        //Меняем время и температуру
          if(!cooking){
             Select(300,42);
             bot.sendMessage(chat_id, "Ви обрали: напій “Ацидолакт”", "Markdown");
          }
-        //Дополнить алгоритмом выбора фун. части
         else{
-            bot.sendMessage(chat_id, "Зачекайте поки приготується попереднє замовлення", "Markdown");
+            bot.sendMessage(chat_id, "Зачекайте, поки приготується попереднє замовлення", "Markdown");
         }
       }
       else if(text.indexOf("Bifilin") != -1){
-        //Меняем время и температуру
         if(!cooking){
             Select(360,44);
             bot.sendMessage(chat_id, "Ви обрали: напій “Біфілін”", "Markdown");
         }
-        //Дополнить алгоритмом выбора фун. части
         else{
-            bot.sendMessage(chat_id, "Зачекайте поки приготується попереднє замовлення", "Markdown");
+            bot.sendMessage(chat_id, "Зачекайте, поки приготується попереднє замовлення", "Markdown");
         }
       }
       else if(text.indexOf("Herolact") != -1){
-        //Меняем время и температуру
          if(!cooking){
             Select(540,37);
             bot.sendMessage(chat_id, "Ви обрали: напій “Геролакт”", "Markdown");
          }
-        //Дополнить алгоритмом выбора фун. части
         else{
-            bot.sendMessage(chat_id, "Зачекайте поки приготується попереднє замовлення", "Markdown");
+            bot.sendMessage(chat_id, "Зачекайте, поки приготується попереднє замовлення", "Markdown");
         }
       }
       else{
@@ -168,39 +154,76 @@ void handleNewMessages(int numNewMessages){
     
     
     if(text.indexOf("/about") != -1){
-      
+      String info = "";
       if(text.indexOf("Bifidok") != -1){
-        
         bot.sendMessage(chat_id, "“Біфідок”", "Markdown");
+        info = "Кисломолочный продукт, що його отримують із незбираного або зі знежиреного коров’ячого молока шляхом кисломолочного бродіння із застосуванням кефірних «грибків».\n";
+        info += "Для приготувавання цього напою Вам потрібно:\n";
+        info += "покупне коров’яче молоко та рідкі біфідобактерії, які продаються в будь-якій аптеці в сухому вигляді.\n";
+        info += "Ампулу такого засобу розводять кип’яченою водою і додають в склянку з прокип’яченим молоком.";
+        bot.sendMessage(chat_id, "" + info, "Markdown");
+        info = "Вся технологія зав’язана на дотриманні температурного режиму, що не перевищує +22 градуса за Цельсієм.";
+        bot.sendMessage(chat_id, "" + info, "Markdown");
+        bot.sendMessage(chat_id, "Час приготування: 10 годин.", "Markdown");             
       }
       else if(text.indexOf("Acidophilin") != -1){
-                
         bot.sendMessage(chat_id, "“Ацидофілін”", "Markdown");
+        info = "Дієтичний кисломолочний продукт, що отримують шляхом збродження молока ацидофільною паличкою.\n";
+        info += "Для приготувавання цього напою Вам потрібно:\n";
+        info += "Коров’яче, козяче або овече молоко, суміш для заквашування, цукр та ягоди.";
+        bot.sendMessage(chat_id, "" + info, "Markdown");
+        info = "Молоко квасять при нагріванні до 32 градусів.";
+        bot.sendMessage(chat_id, "" + info, "Markdown");
+        bot.sendMessage(chat_id, "Триває цей процес близько 5 годин.", "Markdown");                                                    
       }
       else if(text.indexOf("Ryazhenko") != -1){
-        
-        //Дополнить алгоритмом выбора фун. части
         bot.sendMessage(chat_id, "“Ряжанка”", "Markdown");
+        info = "Традиційний молочний продукт української кухні, що виготовляється з пряженого молока.\n";
+        info += "Для приготувавання цього напою Вам потрібно:\n";
+        info += "Пряжене молоко, закваска.";
+        bot.sendMessage(chat_id, "" + info, "Markdown");
+        info = "Режим працює до 42 градусів.";
+        bot.sendMessage(chat_id, "" + info, "Markdown");
+        bot.sendMessage(chat_id, "Ряжанка потребує 5 годин приготування.", "Markdown");        
       }
       else if(text.indexOf("BioYogurt") != -1){
-        
-        //Дополнить алгоритмом выбора фун. части
         bot.sendMessage(chat_id, "“Біойогурт”", "Markdown");
+        info = "Цілющі пробіотики які мають лікувальний ефект, як і усі кисломолочні та біопродукти.\n";
+        info += "Для приготувавання цього напою Вам потрібно:\n";
+        info += "Молоко, закваска, фрукти.";
+        bot.sendMessage(chat_id, "" + info, "Markdown");
+        info = "Для приготування біойогурта температура досягає 42 градусів.";
+        bot.sendMessage(chat_id, "" + info, "Markdown");
+        bot.sendMessage(chat_id, "Один із найшвидких для приготування напій - усього 3 години.", "Markdown");
       }
       else if(text.indexOf("Acidolact") != -1){
-        
-        //Дополнить алгоритмом выбора фун. части
         bot.sendMessage(chat_id, "“Ацидолакт”", "Markdown");
+        info = "продукт для підтримки мікрофлори людям, які приймають антибіотики та інші ліки.\n";
+        info += "Для приготувавання цього напою Вам потрібно:\n";
+        info += "Молоко, закваска, фрукти.";
+        bot.sendMessage(chat_id, "" + info, "Markdown");
+        info = "Напій готується на високій температурі, а саме до 42 градусів.";
+        bot.sendMessage(chat_id, "Вже через 5 годин Ацидолакт буде готовий до споживання.", "Markdown");
       }
       else if(text.indexOf("Bifilin") != -1){
-                
-        //Дополнить алгоритмом выбора фун. части
         bot.sendMessage(chat_id, "“Біфілін”", "Markdown");
+        info = "Кисломолочний продукт, який ідеально підійде для підтримки сил та зміцнення імунітету.\n";
+        info += "Для приготувавання цього напою Вам потрібно:\n";
+        info += "Суха молочна суміш, вода, закваска.";
+        bot.sendMessage(chat_id, "" + info, "Markdown");
+        info = "Біфілін готується при 44 градусів Цельсія.";
+        bot.sendMessage(chat_id, "" + info, "Markdown");
+        bot.sendMessage(chat_id, "Зачекайте 6 годин, і Ви отримаєте це чудовий напій.", "Markdown");        
       }
       else if(text.indexOf("Herolact") != -1){
-        
-        //Дополнить алгоритмом выбора фун. части
         bot.sendMessage(chat_id, "“Геролакт”", "Markdown");
+        info = "Геролакт відрізняється від інших кисломолочних напоїв особливістю складу його мікрофлори та компонентного складу рецептури.\n";
+        info += "Для приготувавання цього напою Вам потрібно:\n";
+        info += "Молоко, закваска стрептосан.";
+        bot.sendMessage(chat_id, "" + info, "Markdown");
+        info = "";
+        bot.sendMessage(chat_id, "Температура цього режиму досягає 37 градусів." + info, "Markdown");
+        bot.sendMessage(chat_id, "І готується Геролакт 9 годин.", "Markdown");
       }
       else{
         bot.sendMessage(chat_id, "Поки що такого рецепту не існує :(", "Markdown");
@@ -210,22 +233,19 @@ void handleNewMessages(int numNewMessages){
         //bot.sendMessage(chat_id, "Поки що такого команди немає :(", "Markdown");
       //}
   }
- }
-
-///////
-///////
+}
 
 void setup() {
 
   Serial.begin(115200);
   pinMode(HEATER, OUTPUT);
-  DS18B20.begin();    // initialize the DS18B20 sensor 
-  digitalWrite(HEATER, HIGH);
-  //
+  DS18B20.begin();// initialize the DS18B20 sensor 
+  //digitalWrite(HEATER, HIGH);
+
   cooking = false;
   cookingTime = 0;
   cookingT = 0;
-  //
+
   Serial.print("Connecting to Wifi SSID ");
   Serial.print(WIFI_SSID);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
@@ -283,7 +303,6 @@ void loop() {
         }
       }
     }
-    //
     if(millis() - bot_lasttime > BOT_MTBS)
     {
     int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
@@ -294,7 +313,6 @@ void loop() {
       handleNewMessages(numNewMessages);
       numNewMessages = bot.getUpdates(bot.last_message_received + 1);
     }
-
     bot_lasttime = millis();
   }
 }
